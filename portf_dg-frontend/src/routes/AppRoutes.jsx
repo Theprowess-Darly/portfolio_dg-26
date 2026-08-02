2f16ee9ccd2dd9ac096b93ebae9e2e82e227d05b
DARLY GORELLE <mrsdianghadg@gmail.com>
2026-08-02 21:12:18 +0100

    going to the frontend now

---
+++ portf_dg-frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+function Home() {
+    return <h1>Home</h1>
+}
+function About() {
+    return <h1>About</h1>
+}
+function Projects() {
+    return <h1>Projects</h1>
+}
+function ProjectDetails() {
+    return <h1>Project Details</h1>
+}
+function Blog() {
+    return <h1>Blog</h1>
+}
+function BlogDetails() {
+    return <h1>Blog Details</h1>
+}
+function Contact() {
+    return <h1>Contact</h1>
+}
+
+function AppRoutes() {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/about" element={<About />}/>
+                <Route path="/projects" element={<Projects />}/>
+                <Route path="/projects/:slug" element={<ProjectDetails />}/>
+                <Route path="/blog" element={<Blog />}/>
+                <Route path="/blog/:slug" element={<BlogDetails />}/>
+                <Route path="/contact" element={<Contact />}/>
+
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRoutes
